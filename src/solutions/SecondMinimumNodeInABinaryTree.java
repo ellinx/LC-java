01507e565dd79afdf58a399e6e0c27557bec5ef4
ellinx <ellin.xll@gmail.com>
2018-06-10 10:53:08 -0700

    Add Second Minimum Node In a Binary Tree.

---
+++ src/solutions/SecondMinimumNodeInABinaryTree.java
@@ -0,0 +1,71 @@
+package solutions;
+
+/**
+ * 
+ *  Given a non-empty special binary tree consisting of nodes with the non-negative value, 
+ *  where each node in this tree has exactly two or zero sub-node. If the node has two sub-nodes, 
+ *  then this node's value is the smaller value among its two sub-nodes.
+ *  
+ *  Given such a binary tree, you need to output the second minimum value in the set made of 
+ *  all the nodes' value in the whole tree.
+ *  
+ *  If no such second minimum value exists, output -1 instead.
+ *  
+ *  Example 1:
+ *  
+ *  Input: 
+ *      2
+ *     / \
+ *    2   5
+ *       / \
+ *      5   7
+ *  
+ *  Output: 5
+ *  Explanation: The smallest value is 2, the second smallest value is 5.
+ *  
+ *  Example 2:
+ *  
+ *  Input: 
+ *      2
+ *     / \
+ *    2   2
+ *  
+ *  Output: -1
+ *  Explanation: The smallest value is 2, but there isn't any second smallest value.
+ *  
+ * @author Ellinx
+ *
+ */
+public class SecondMinimumNodeInABinaryTree {
+	public int findSecondMinimumValue(TreeNode root) {
+        if (root.left==null && root.right==null) {
+            return -1;
+        }
+        if (root.left.val>root.right.val) {
+            int other = findSecondMinimumValue(root.right);
+            if (other>root.val && other<root.left.val) {
+                return other;
+            }
+            return root.left.val;
+        } else if (root.left.val<root.right.val) {
+            int other = findSecondMinimumValue(root.left);
+            if (other>root.val && other<root.right.val) {
+                return other;
+            }
+            return root.right.val;
+        }
+        int left = findSecondMinimumValue(root.left);
+        int right = findSecondMinimumValue(root.right);
+        //System.out.println(left+","+right);
+        if (left>root.val && right>root.val) {
+            return Math.min(left,right);
+        }
+        if (left>root.val) {
+            return left;
+        }
+        if (right>root.val) {
+            return right;
+        }
+        return -1;
+    }
+}
