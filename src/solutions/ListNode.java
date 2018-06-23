bd7de8dcdf69bb6ec1a9e2b172448db5feab3dad
ellinx <ellin.xll@gmail.com>
2018-06-23 15:04:37 -0700

    Update ListNode.

--- src/solutions/ListNode.java
+++ src/solutions/ListNode.java
@@ -4,4 +4,19 @@ public class ListNode {
 	int val;
 	ListNode next;
 	ListNode(int x) { val = x; }
+	
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(val);
+		ListNode cur = next;
+		while (cur!=null) {
+			sb.append(",").append(cur.val);
+			cur = cur.next;
+		}
+		return sb.toString();
+	}
+	
+	
 }
