a916e90a0658de68383426c56716a45dae6dc9ca
ellinx <ellin.xll@gmail.com>
2018-07-15 20:19:09 -0700

    Update Interleaving String.

--- src/solutions/InterleavingString.java
+++ src/solutions/InterleavingString.java
@@ -1,5 +1,8 @@
 package solutions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Given s1, s2, s3, find whether s3 is formed by the interleaving of s1 and s2.
  * 
@@ -13,6 +16,11 @@ package solutions;
  *
  */
 public class InterleavingString {
+	/**
+	 * Thoughts:
+	 * 1. dynamic programming
+	 * 
+	 */
 	public boolean isInterleave(String s1, String s2, String s3) {
 		if (s1.length() + s2.length() != s3.length())
 			return false;
@@ -38,6 +46,46 @@ public class InterleavingString {
 		}
 		return dp[s1.length()][s2.length()];
 	}
+	
+	/**
+	 * Thoughts:
+	 * 1. DFS
+	 */
+    private Map<String,Boolean> map = new HashMap<>();
+    public boolean isInterleaveDFS(String s1, String s2, String s3) {
+        //System.out.println(s1+","+s2+","+s3);
+        if (s1.length()+s2.length()!=s3.length()) {
+            return false;
+        }
+        if (s3.length()==0) {
+            return true;
+        }
+        if (s1.length()==0) {
+            return s2.equals(s3);
+        }
+        if (map.containsKey(s1+","+s2+","+s3)) {
+            return map.get(s1+","+s2+","+s3);
+        }
+        for (int i=0;i<s3.length();i++) {
+            if (s1.charAt(0)==s3.charAt(i)) {
+                if (i==0) {
+                    if (isInterleave(s1.substring(1),s2,s3.substring(1))) {
+                        map.put(s1+","+s2+","+s3, true);
+                        return true;
+                    }
+                } else {
+                    if (s2.length()>=i && s2.substring(0,i).equals(s3.substring(0,i))) {
+                        if (isInterleave(s1.substring(1),s2.substring(i),s3.substring(i+1))) {
+                            map.put(s1+","+s2+","+s3, true);
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        map.put(s1+","+s2+","+s3, false);
+        return false;
+    }
 	
 	//test
 	public static void main(String[] args) {
