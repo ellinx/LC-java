8410bd4be2b483193097a25cb9de204e2fb997e6
ellinx <ellin.xll@gmail.com>
2018-08-19 13:14:53 -0700

    Update Longest Increasing Subsequence.

--- src/solutions/LongestIncreasingSubsequence.java
+++ src/solutions/LongestIncreasingSubsequence.java
@@ -21,25 +21,37 @@ import java.util.List;
  *
  */
 public class LongestIncreasingSubsequence {
-	public int lengthOfLIS(int[] nums) {
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length<=1) {
+            return nums.length;
+        }
         List<Integer> list = new ArrayList<>();
-        for (int num:nums) {
-            if (list.isEmpty()) {
-                list.add(num);
-                continue;
+        list.add(nums[0]);
+        for (int i=1;i<nums.length;i++) {
+            int index = insertIndex(list, nums[i]);
+            if (index==list.size()) {
+                list.add(nums[i]);
+            } else {
+                list.set(index, nums[i]);
             }
-            int index = 0;
-            while (index<list.size()) {
-                if (list.get(index)>=num) {
-                    list.set(index, num);
-                    break;
-                }
-                index++;
-            }
-            if (index==list.size())
-                list.add(num);
         }
         return list.size();
+    }
+    
+    private int insertIndex(List<Integer> list, int num) {
+        int start=0, end=list.size()-1;
+        while (start<=end) {
+            int mid = start+(end-start)/2;
+            if (list.get(mid)==num) {
+                return mid;
+            }
+            if (list.get(mid)<num) {
+                start = mid+1;
+            } else {
+                end = mid-1;
+            }
+        }
+        return start;
     }
 	
 	//test
