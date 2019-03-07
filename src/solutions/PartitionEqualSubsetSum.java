c257ddd838fa17f53c4573a21d04263de3d4a9ca
ellinx <ellin.xll@gmail.com>
2019-03-06 21:43:06 -0800

    Update Partition Equal Subset Sum.

--- src/solutions/PartitionEqualSubsetSum.java
+++ src/solutions/PartitionEqualSubsetSum.java
@@ -1,6 +1,8 @@
 package solutions;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Given a non-empty array containing only positive integers, find if the array
@@ -26,35 +28,61 @@ import java.util.Arrays;
  *
  */
 public class PartitionEqualSubsetSum {
-	public boolean canPartition(int[] nums) {
-		int n = nums.length;
-        int sum = 0;
-        
-        for (int i=0;i<n;i++) {
-        	sum += nums[i];
+	// dp
+    public boolean canPartition(int[] nums) {
+        int total = 0;
+        for (int num:nums) {
+            total += num;
         }
-        
-        if (sum%2==1) return false;
-        sum = sum>>1;
-        
-        boolean[][] dp = new boolean[n+1][sum+1];
-        
-        for (int i=0;i<n+1;i++) {
-        	dp[i][0] = true;
+        if (total%2==1) {
+            return false;
         }
-        
-        for (int i=1;i<=n;i++) {
-        	for (int j=1;j<=sum;j++) {
-        		dp[i][j] = dp[i-1][j];
-        		if (nums[i-1]<=j) {
-        			dp[i][j] = dp[i][j] || dp[i-1][j-nums[i-1]];
-        		}
-        	}
+        int m = nums.length;
+        int n = total/2;
+        //dp[i][j] is whether can get subset sum equal j from first i elements
+        boolean[][] dp = new boolean[m+1][n+1];
+        dp[0][0] = true;
+        for (int i=1;i<=m;i++) {
+            for (int j=1;j<=n;j++) {
+                dp[i][j] = dp[i-1][j];
+                if (!dp[i][j] && j-nums[i-1]>=0) {
+                    dp[i][j] = dp[i-1][j-nums[i-1]];
+                }
+            }
         }
-        for (int i=0;i<=n;i++) {
-        	System.out.println(Arrays.toString(dp[i]));
+        return dp[m][n];
+    }
+    
+    // dfs + memorization
+    public boolean canPartition2(int[] nums) {
+        int N = nums.length;
+        int total = 0;
+        for (int num:nums) {
+            total += num;
         }
-        return dp[n][sum];
+        if (total%2==1) {
+            return false;
+        }
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+        return dfs(nums, 0, 0, total/2, new HashMap<String,Boolean>());
+    }
+    private boolean dfs(int[] nums, int start, int cur, int target, Map<String,Boolean> mm) {
+        if (cur==target) {
+            return true;
+        }
+        String key = start+","+cur;
+        if (mm.containsKey(key)) {
+            return mm.get(key);
+        }
+        for (int i=start;i<nums.length;i++) {
+            if (cur+nums[i]<=target && dfs(nums, i+1, cur+nums[i], target, mm)) {
+                mm.put(key, true);
+                return true;
+            }
+        }
+        mm.put(key, false);
+        return false;
     }
 	
 	//test
