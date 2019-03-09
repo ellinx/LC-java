bd37ac06c21bc9c72a9f3982df62b4132dd80615
ellinx <ellin.xll@gmail.com>
2019-03-08 21:13:37 -0800

    Update Bulls and Cows.

--- src/solutions/BullsAndCows.java
+++ src/solutions/BullsAndCows.java
@@ -1,14 +1,25 @@
 package solutions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * You are playing the following Bulls and Cows game with your friend: You write down a number and ask your friend to guess what the number is. Each time your friend makes a guess, you provide a hint that indicates how many digits in said guess match your secret number exactly in both digit and position (called "bulls") and how many digits match the secret number but locate in the wrong position (called "cows"). Your friend will use successive guesses and hints to eventually derive the secret number.
+ * You are playing the following Bulls and Cows game with your friend: 
+ * You write down a number and ask your friend to guess what the number is. 
+ * Each time your friend makes a guess, you provide a hint that indicates 
+ * how many digits in said guess match your secret number exactly in both digit and position (called "bulls") 
+ * and how many digits match the secret number but locate in the wrong position (called "cows"). 
+ * Your friend will use successive guesses and hints to eventually derive the secret number.
  * 
  * For example:
  * 
  * Secret number:  "1807"
  * Friend's guess: "7810"
  * Hint: 1 bull and 3 cows. (The bull is 8, the cows are 0, 1 and 7.)
- * Write a function to return a hint according to the secret number and friend's guess, use A to indicate the bulls and B to indicate the cows. In the above example, your function should return "1A3B".
+ * 
+ * Write a function to return a hint according to the secret number and friend's guess, 
+ * use A to indicate the bulls and B to indicate the cows. 
+ * In the above example, your function should return "1A3B".
  * 
  * Please note that both secret number and friend's guess may contain duplicate digits, for example:
  * 
@@ -16,36 +27,30 @@ package solutions;
  * Friend's guess: "0111"
  * In this case, the 1st 1 in friend's guess is a bull, the 2nd or 3rd 1 is a cow, and your function should return "1A1B".
  * You may assume that the secret number and your friend's guess only contain digits, and their lengths are always equal.
- * 
- * @author Ellinx
- *
  */
 public class BullsAndCows {
-	public String getHint(String secret, String guess) {
-        int[] count = new int[10];
-        int n = secret.length();
-        
-        for (int i=0;i<n;i++) {
-        	count[secret.charAt(i)-'0']++;
+    public String getHint(String secret, String guess) {
+        Map<Character,Integer> counter = new HashMap<>();
+        for (char c:secret.toCharArray()) {
+            counter.put(c, counter.getOrDefault(c,0)+1);
         }
-        
-        int A = 0;
-        int B = 0;
-        for (int i=0;i<n;i++) {
-        	if (secret.charAt(i) == guess.charAt(i)) {
-        		count[secret.charAt(i)-'0']--;
-        		A++;
-        	}
+        int cntA=0, cntB=0;
+        for (int i=0;i<guess.length();i++) {
+            if (guess.charAt(i)==secret.charAt(i)) {
+                counter.put(secret.charAt(i), counter.get(secret.charAt(i))-1);
+                cntA++;
+            }
         }
-        
-        for (int i=0;i<n;i++) {
-        	if (secret.charAt(i) != guess.charAt(i) && count[guess.charAt(i)-'0']>0) {
-        		count[guess.charAt(i)-'0']--;
-        		B++;
-        	}
+        for (int i=0;i<guess.length();i++) {
+            if (guess.charAt(i)!=secret.charAt(i)) {
+                int temp = counter.getOrDefault(guess.charAt(i),0);
+                if (temp>0) {
+                    counter.put(guess.charAt(i), temp-1);
+                    cntB++;
+                }
+            }
         }
-        
-        return Integer.toString(A)+"A"+Integer.toString(B)+"B";
+        return cntA+"A"+cntB+"B";
     }
 	
 	//test
