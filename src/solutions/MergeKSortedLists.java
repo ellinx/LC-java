b921f8c10e682099eaa9151d783908ebfb7d58a7
ellinx <ellin.xll@gmail.com>
2019-03-09 23:26:22 -0700

    Update Merge k Sorted Lists.

--- src/solutions/MergeKSortedLists.java
+++ src/solutions/MergeKSortedLists.java
@@ -4,41 +4,50 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 /**
- * Merge k sorted linked lists and return it as one sorted list. 
- * Analyze and describe its complexity.
- * @author Ellinx
- *
+Merge k sorted linked lists and return it as one sorted list. 
+Analyze and describe its complexity.
+
+Example:
+Input:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+Output: 1->1->2->3->4->4->5->6
  */
 public class MergeKSortedLists {
-	public ListNode mergeKLists(ListNode[] lists) {
-        ListNode head = null;
-        ListNode cur = null;
-        PriorityQueue<ListNode> pq = new PriorityQueue<>(new Comparator<ListNode>(){
-            public int compare(ListNode a, ListNode b) {
-                return a.val-b.val;
+    class Pair {
+        int index;
+        ListNode node;
+        
+        public Pair(int index, ListNode node) {
+            this.index = index;
+            this.node = node;
+        }
+    }
+    
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<Pair> pq = new PriorityQueue<Pair>(new Comparator<Pair>(){
+            public int compare(Pair a, Pair b) {
+                return a.node.val-b.node.val;
             }
         });
-        
-        for (ListNode list:lists) {
-            if (list!=null)
-                pq.add(list);
-
+        for (int i=0;i<lists.length;i++) {
+            if (lists[i]!=null) {
+                pq.offer(new Pair(i,lists[i]));
+            }
         }
-        
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
         while (!pq.isEmpty()) {
-            ListNode node = pq.poll();
-            if (cur==null) {
-                cur = node;
-                head = node;
-            } else {
-                cur.next = node;
-                cur = cur.next;
-            }
-            node = node.next;
-            if (node!=null) {
-                pq.add(node);
+            Pair temp = pq.poll();
+            cur.next = temp.node;
+            cur = cur.next;
+            if (temp.node.next!=null) {
+                pq.offer(new Pair(temp.index, temp.node.next));
             }
         }
-        return head;
+        return dummy.next;
     }
 }
