9c629aec9d0e6d4d0dbd3cf66e86a1793cf04dc8
ellinx <ellin.xll@gmail.com>
2019-03-09 23:12:46 -0700

    Update Word Break.

--- src/solutions/WordBreak.java
+++ src/solutions/WordBreak.java
@@ -1,97 +1,55 @@
 package solutions;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
+
+/**
+Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, 
+determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+Note:
+1. The same word in the dictionary may be reused multiple times in the segmentation.
+2. You may assume the dictionary does not contain duplicate words.
+
+Example 1:
+Input: s = "leetcode", wordDict = ["leet", "code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+
+Example 2:
+Input: s = "applepenapple", wordDict = ["apple", "pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+             Note that you are allowed to reuse a dictionary word.
+             
+Example 3:
+Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+Output: false
+ */
 
 public class WordBreak {
-	/**
-	 * Given a non-empty string s and a dictionary wordDict containing a list of
-	 * non-empty words, determine if s can be segmented into a space-separated
-	 * sequence of one or more dictionary words. You may assume the dictionary
-	 * does not contain duplicate words.
-	 * 
-	 * For example, given
-	 * s = "leetcode", 
-	 * dict = ["leet", "code"].
-	 * 
-	 * Return true because "leetcode" can be segmented as "leet code".
-	 */
-	public boolean wordBreak(String s, List<String> wordDict) {
-		int[] dp = new int[s.length()+1];
-        Set<String> wordSet = new HashSet<>();
-        //initialize dp array
-        dp[0] = 1;
-        //initialize word set
-        for (String word : wordDict) {
-            wordSet.add(word);
-        }
-        
-        for (int end=0;end<s.length();end++) {
-            for (int start=0;start<=end;start++) {
-                if (dp[start]==1) {
-                    String cur = s.substring(start, end+1);
-                    if (wordSet.contains(cur)) {
-                        dp[end+1] = 1;
-                        break;
-                    }
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        boolean[] dp = new boolean[n+1];
+        dp[0] = true;
+        for (int i=0;i<=n;i++) {
+            if (dp[n]) {
+                return true;
+            }
+            if (!dp[i]) {
+                continue;
+            }
+            for (String word:wordDict) {
+                int m = word.length();
+                if (i+m<=n && s.substring(i,i+m).equals(word)) {
+                    dp[i+m] = true;
                 }
             }
         }
-        return (dp[s.length()]==1);
+        // System.out.println(Arrays.toString(dp));
+        return dp[n];
     }
-	
-	
-	/**
-	 * Given a non-empty string s and a dictionary wordDict containing a list of 
-	 * non-empty words, add spaces in s to construct a sentence where each word 
-	 * is a valid dictionary word. You may assume the dictionary does not contain 
-	 * duplicate words.
-	 * 
-	 * Return all such possible sentences.
-	 * 
-	 * For example, given
-	 * s = "catsanddog",
-	 * dict = ["cat", "cats", "and", "sand", "dog"].
-	 * 
-	 * A solution is ["cats and dog", "cat sand dog"].
-	 */
-	public List<String> wordBreakII(String s, List<String> wordDict) {
-		Set<String> set = new HashSet<>();
-        for (String str:wordDict) {
-            set.add(str);
-        }
-        return DFS(s,set,new HashMap<String, List<String>>());
-    }
-	private List<String> DFS(String s, Set<String> set, Map<String, List<String>> map) {
-        if (map.containsKey(s))
-            return map.get(s);
-        
-        List<String> res = new LinkedList<>();
-        for (int i=1;i<=s.length();i++) {
-            String left = s.substring(0,i);
-            if (set.contains(left)) {
-                if (i<s.length()) {
-                    List<String> right = DFS(s.substring(i), set, map);
-                    if (!right.isEmpty()) {
-                        for (String str:right) {
-                            res.add(left+" "+str);
-                        }
-                    }
-                } else {
-                    res.add(left);
-                }
-            }
-        }
-        
-        map.put(s, res);
-        return res;
-    }
+
 	
 	//test
 	public static void main(String[] args) {
@@ -103,10 +61,7 @@ public class WordBreak {
 		list.add("sand");
 		list.add("and");
 		list.add("dog");
-		List<String> result = wb.wordBreakII(s, list);
-		//System.out.println("result is "+result);
-		for (String str:result) {
-			System.out.println(str);
-		}
+		boolean result = wb.wordBreak(s, list);
+		System.out.println("result is "+result);
 	}
 }
