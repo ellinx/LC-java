c838fb4bb7a3834af3e49c6f1a4b77315925f820
ellinx <ellin.xll@gmail.com>
2019-03-10 21:55:39 -0700

    Update Top K Frequent Elements.

--- src/solutions/TopKFrequentElements.java
+++ src/solutions/TopKFrequentElements.java
@@ -8,46 +8,42 @@ import java.util.Map;
 import java.util.PriorityQueue;
 
 /**
- * Given a non-empty array of integers, return the k most frequent elements.
- * 
- * For example,
- * Given [1,1,1,2,2,3] and k = 2, return [1,2].
- * 
- * Note: 
- * You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
- * Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
- * 
- * @author Ellinx
- *
+Given a non-empty array of integers, return the k most frequent elements.
+
+Example 1:
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+
+Example 2:
+Input: nums = [1], k = 1
+Output: [1]
+
+Note:
+1. You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+2. Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
  */
 public class TopKFrequentElements {
-	public List<Integer> topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        PriorityQueue<Map.Entry<Integer,Integer>> maxHeap = 
-        		new PriorityQueue<>(
-        				(Map.Entry<Integer,Integer> e1, Map.Entry<Integer,Integer> e2)->
-        						(e2.getValue()-e1.getValue())
-        				);
-        
-        List<Integer> res = new ArrayList<>();
-        
-        for (int num : nums) {
-        	if (map.containsKey(num)) {
-        		map.put(num, map.get(num)+1);
-        	} else {
-        		map.put(num, 1);
-        	}
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        Map<Integer,Integer> counter = new HashMap<>();
+        for (int num:nums) {
+            counter.put(num, counter.getOrDefault(num,0)+1);
         }
-        
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-        	maxHeap.offer(entry);
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>(){
+            public int compare(int[] a, int[] b) {
+                return a[1]-b[1];
+            }
+        });
+        for (int key:counter.keySet()) {
+            pq.offer(new int[]{key,counter.get(key)});
+            if (pq.size()>k) {
+                pq.poll();
+            }
         }
-        
-        for (int i=0;i<k;i++) {
-        	res.add(maxHeap.poll().getKey());
+        List<Integer> ret = new ArrayList<>();
+        while (!pq.isEmpty()) {
+            ret.add(pq.poll()[0]);
         }
-        
-        return res;
+        return ret;
     }
 	
 	public static void main(String[] args) {
@@ -55,12 +51,5 @@ public class TopKFrequentElements {
 		int[] nums = {1,1,1,2,2,3};
 		List<Integer> result = tmp.topKFrequent(nums, 2);
 		System.out.println(result.toString());
-		
-		PriorityQueue<String> minHeap = 
-				new PriorityQueue<>(10, new Comparator<String>(){
-					public int compare(String s1, String s2) {
-						return Integer.compare(s1.length(), s2.length());
-					}
-				});
 	}
 }
