e1ee7281617f0c757e9503be87c2d273fe4cec15
ellinx <ellin.xll@gmail.com>
2019-03-10 22:14:59 -0700

    Update Group Anagrams.

--- src/solutions/GroupAnagrams.java
+++ src/solutions/GroupAnagrams.java
@@ -7,32 +7,33 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Given an array of strings, group anagrams together.
- * 
- * For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"], 
- * 
- * Return:
- * 
- * [ 
- * 		["ate", "eat","tea"], 
- * 		["nat","tan"], 
- * 		["bat"] 
- * ] 
- * Note: All inputs will be in lower-case.
- * 
- * @author Ellinx
- *
+Given an array of strings, group anagrams together.
+
+Example:
+
+Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+Output:
+[
+  ["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+
+Note:
+1. All inputs will be in lowercase.
+2. The order of your output does not matter.
  */
 public class GroupAnagrams {
-	public List<List<String>> groupAnagrams(String[] strs) {
-		Map<String, List<String>> map = new HashMap<>();
-        for (String str:strs) {
-            char[] arr = str.toCharArray();
-            Arrays.sort(arr);
-            String key = new String(arr);
-            if (!map.containsKey(key))
-                map.put(key, new ArrayList<String>());
-            map.get(key).add(str);
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> map = new HashMap<>();
+        for (String s:strs) {
+            char[] key = s.toCharArray();
+            Arrays.sort(key);
+            String k = new String(key);
+            if (!map.containsKey(k)) {
+                map.put(k, new ArrayList<String>());
+            }
+            map.get(k).add(s);
         }
         List<List<String>> ret = new ArrayList<>();
         for (String k:map.keySet()) {
