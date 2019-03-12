e5c645d048d3783792d586cd2410e4a2da958a16
ellinx <ellin.xll@gmail.com>
2019-03-11 20:52:02 -0700

    Update Meeting Rooms.

--- src/solutions/MeetingRooms.java
+++ src/solutions/MeetingRooms.java
@@ -4,36 +4,26 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 /**
- * Given an array of meeting time intervals consisting of start and end times
- * [[s1,e1],[s2,e2],...] (si < ei), determine if a person could attend all
- * meetings.
- * 
- * Example 1:
- * 
- * Input: [[0,30],[5,10],[15,20]] 
- * Output: false
- * 
- * Example 2:
- * 
- * Input: [[7,10],[2,4]] 
- * Output: true
- * 
- * 
- * @author Ellinx
- *
+Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), 
+determine if a person could attend all meetings.
+
+Example 1:
+Input: [[0,30],[5,10],[15,20]]
+Output: false
+
+Example 2:
+Input: [[7,10],[2,4]]
+Output: true
  */
 public class MeetingRooms {
     public boolean canAttendMeetings(Interval[] intervals) {
         Arrays.sort(intervals, new Comparator<Interval>(){
-            public int compare(Interval i1, Interval i2) {
-                if (i1.start!=i2.start) {
-                    return i1.start-i2.start;
-                }
-                return i1.end-i2.end;
+            public int compare(Interval a, Interval b) {
+                return a.start-b.start;
             }
         });
-        for (int i=1;i<intervals.length;i++) {
-            if (intervals[i-1].end>intervals[i].start) {
+        for (int i=0;i<intervals.length-1;i++) {
+            if (intervals[i].end>intervals[i+1].start) {
                 return false;
             }
         }
