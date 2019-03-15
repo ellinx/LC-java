d60daeefe99cce16b21e4a3523f8fcb43fc98f34
ellinx <ellin.xll@gmail.com>
2019-03-14 19:48:08 -0700

    Update Basic Calculator.

--- src/solutions/BasicCalculator.java
+++ src/solutions/BasicCalculator.java
@@ -2,131 +2,80 @@ package solutions;
 
 import java.util.Stack;
 
+/**
+Implement a basic calculator to evaluate a simple expression string.
+
+The expression string may contain open ( and closing parentheses ), 
+the plus + or minus sign -, non-negative integers and empty spaces .
+
+Example 1:
+Input: "1 + 1"
+Output: 2
+
+Example 2:
+Input: " 2-1 + 2 "
+Output: 3
+
+Example 3:
+Input: "(1+(4+5+2)-3)+(6+8)"
+Output: 23
+
+Note:
+1. You may assume that the given expression is always valid.
+2. Do not use the eval built-in library function.
+ */
 public class BasicCalculator {
-	/**
-	 * Implement a basic calculator to evaluate a simple expression string.
-	 * 
-	 * The expression string may contain open ( and closing parentheses ), the plus
-	 * + or minus sign -, non-negative integers and empty spaces .
-	 * 
-	 * You may assume that the given expression is always valid.
-	 * 
-	 * Some examples: 
-	 * "1 + 1" = 2 
-	 * " 2-1 + 2 " = 3 
-	 * "(1+(4+5+2)-3)+(6+8)" = 23 
-	 * 
-	 * Note:
-	 * Do not use the eval built-in library function.
-	 * 
-	 *
-	 */
-	public int calculateI(String s) {
-		int number = 0;
-        int res = 0;
-        int sign = 1;
-        int index = 0;
-        
-        while (index < s.length()) {
-            char a = s.charAt(index);
-            if (Character.isDigit(a)) {
-                number = 10*number+(a-'0');
-            } else if (a=='+') {
-                res += sign*number;
+    public int calculate(String s) {
+        Stack<Integer> stk = new Stack<>();
+        int l=0, r=0;
+        int ret=0, sign=1;
+        while (r<s.length()) {
+            while (r<s.length() && s.charAt(r)==' ') {
+                r++;
+            }
+            if (r==s.length()) {
+                break;
+            }
+            if (s.charAt(r)=='+') {
                 sign = 1;
-                number = 0;
-            } else if (a=='-') {
-                res += sign*number;
+                r++;
+                l = r;
+            } else if (s.charAt(r)=='-') {
                 sign = -1;
-                number = 0;
-            } else if (a=='(') {
-                int count = 0;
-                int end = index+1;
-                while (end < s.length()) {
-                    char curChar = s.charAt(end);
-                    if (curChar=='(')
-                        count++;
-                    else if (curChar==')') {
-                        if (count>0)
-                            count--;
-                        else
-                            break;
-                    }
-                    end++;
-                }
-                res += sign*calculateI(s.substring(index+1,end));
+                r++;
+                l = r;
+            } else if (s.charAt(r)=='(') {
+                stk.push(ret);
+                stk.push(sign);
+                ret = 0;
                 sign = 1;
-                number = 0;
-                index = end;
-            }
-            index++;
-        }
-        res += sign*number;
-        return res;
-    }
-	
-	/**
-	 * Implement a basic calculator to evaluate a simple expression string.
-	 * 
-	 * The expression string contains only non-negative integers, +, -, *, / operators 
-	 * and empty spaces . The integer division should truncate toward zero.
-	 * 
-	 * You may assume that the given expression is always valid.
-	 * 
-	 * Some examples:
-	 * "3+2*2" = 7
-	 * " 3/2 " = 1
-	 * " 3+5 / 2 " = 5
-	 * Note: Do not use the eval built-in library function.
-	 */
-	public int calculateII(String s) {
-		char sign = '+';
-        int number = 0;
-        int res = 0;
-        int index = 0;
-        Stack<Integer> stack = new Stack<>();
-        
-        while (index <= s.length()) {
-            if (index < s.length() && Character.isDigit(s.charAt(index))) {
-                number = 10*number+(s.charAt(index)-'0');
-            } else if (index == s.length() || s.charAt(index)=='+' || s.charAt(index)=='-' || s.charAt(index)=='*' || s.charAt(index)=='/') {
-                switch(sign) {
-                    case '+':
-                        stack.push(number);
-                        break;
-                    case '-':
-                        stack.push(-number);
-                        break;
-                    case '*':
-                        stack.push(stack.pop()*number);
-                        break;
-                    case '/':
-                        stack.push(stack.pop()/number);
-                        break;
-                    default:
-                        System.out.println("unknown sign error!");
-                        break;
+                r++;
+                l = r;
+            } else if (s.charAt(r)==')') {
+                int temp = ret;
+                sign = stk.pop();
+                ret = stk.pop();
+                ret += sign*temp;
+                r++;
+                l = r;
+            } else {
+                l = r;
+                r++;
+                while (r<s.length() && s.charAt(r)>='0' && s.charAt(r)<='9') {
+                    r++;
                 }
-                if (index==s.length())
-                    break;
-                
-                sign = s.charAt(index);
-                number = 0;
+                ret += sign*Integer.parseInt(s.substring(l,r));
+                l = r;
             }
-            index++;
         }
-        
-        while (!stack.isEmpty()) {
-            res += stack.pop();
-        }
-        return res;
+        return ret;
     }
 	
 	//test 
 	public static void main(String[] args) {
 		BasicCalculator bc = new BasicCalculator();
-		String s = "3+2*2";
-		int result = bc.calculateII(s);
+		String s = "3+2-1";
+		int result = bc.calculate(s);
 		System.out.println(result);
 	}
 }
