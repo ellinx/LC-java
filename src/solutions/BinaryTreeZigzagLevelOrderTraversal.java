055bcfcc0b7461637c8b15ebc7a8cd9035475aba
ellinx <ellin.xll@gmail.com>
2019-03-14 20:13:59 -0700

    Update Binary Tree Zigzag Level Order Traversal.

--- src/solutions/BinaryTreeZigzagLevelOrderTraversal.java
+++ src/solutions/BinaryTreeZigzagLevelOrderTraversal.java
@@ -5,26 +5,22 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * 
- * Given a binary tree, return the zigzag level order traversal of its nodes' values.
- * (ie, from left to right, then right to left for the next level and alternate between).
- * 
- * For example:
- * Given binary tree [3,9,20,null,null,15,7],
- *     3
- *    / \
- *   9  20
- *     /  \
- *    15   7
- * return its zigzag level order traversal as:
- * [
- *   [3],
- *   [20,9],
- *   [15,7]
- * ]
- * 
- * @author Ellinx
- *
+Given a binary tree, return the zigzag level order traversal of its nodes' values. 
+(ie, from left to right, then right to left for the next level and alternate between).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its zigzag level order traversal as:
+[
+  [3],
+  [20,9],
+  [15,7]
+]
  */
 public class BinaryTreeZigzagLevelOrderTraversal {
 	/**
@@ -34,28 +30,35 @@ public class BinaryTreeZigzagLevelOrderTraversal {
 	 * Time: O(n) where n is total number of nodes in the tree
 	 * Space: O(n)
 	 */
-	public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> ret = new ArrayList<>();
-        List<TreeNode> cur = new ArrayList<>();
-        if (root!=null)
-            cur.add(root);
-        while (cur.size()!=0) {
-            List<TreeNode> next = new ArrayList<>();
-            List<Integer> curInt = new ArrayList<>();
-            for (TreeNode node : cur) {
-                curInt.add(node.val);
-                if (node.left!=null)
-                    next.add(node.left);
-                if (node.right!=null)
-                    next.add(node.right);
-            }
-            ret.add(curInt);
-            cur = next;
+        if (root==null) {
+            return ret;
         }
-        for (int i=0;i<ret.size();i++) {
-            if (i%2==1) {
-                Collections.reverse(ret.get(i));
+        boolean flag = true;
+        List<TreeNode> cur = new ArrayList<>();
+        cur.add(root);
+        while (!cur.isEmpty()) {
+            List<TreeNode> next = new ArrayList<>();
+            List<Integer> vals = new ArrayList<>();
+            for (TreeNode each:cur) {
+                vals.add(each.val);
+                if (each.left!=null) {
+                    next.add(each.left);
+                }
+                if (each.right!=null) {
+                    next.add(each.right);
+                }
             }
+            if (flag) {
+                ret.add(vals);
+                flag = false;
+            } else {
+                Collections.reverse(vals);
+                ret.add(vals);
+                flag = true;
+            }
+            cur = next;
         }
         return ret;
     }
