5da2d9cd5702d7bdf5bf2360ef201e9a75b6307e
ellinx <ellin.xll@gmail.com>
2019-03-14 19:54:25 -0700

    Update Edit Distance.

--- src/solutions/EditDistance.java
+++ src/solutions/EditDistance.java
@@ -1,53 +1,43 @@
 package solutions;
 
 /**
- * 
- * Given two words word1 and word2, find the minimum number of operations
- * required to convert word1 to word2.
- * 
- * You have the following 3 operations permitted on a word:
- * 
- * Insert a character Delete a character Replace a character
- * 
- * Example 1:
- * 
- * Input: word1 = "horse", word2 = "ros" 
- * Output: 3 
- * 
- * Explanation: 
- * horse -> rorse (replace 'h' with 'r') 
- * rorse -> rose (remove 'r') 
- * rose -> ros (remove 'e')
- * 
- * Example 2:
- * 
- * Input: word1 = "intention", word2 = "execution" 
- * Output: 5 
- * 
- * Explanation:
- * intention -> inention (remove 't') 
- * inention -> enention (replace 'i' with 'e') 
- * enention -> exention (replace 'n' with 'x') 
- * exention -> exection (replace 'n' with 'c') 
- * exection -> execution (insert 'u')
- * 
- * 
- * @author Ellinx
- *
+Given two words word1 and word2, 
+find the minimum number of operations required to convert word1 to word2.
+
+You have the following 3 operations permitted on a word:
+
+Insert a character
+Delete a character
+Replace a character
+
+Example 1:
+Input: word1 = "horse", word2 = "ros"
+Output: 3
+Explanation: 
+horse -> rorse (replace 'h' with 'r')
+rorse -> rose (remove 'r')
+rose -> ros (remove 'e')
+
+Example 2:
+Input: word1 = "intention", word2 = "execution"
+Output: 5
+Explanation: 
+intention -> inention (remove 't')
+inention -> enention (replace 'i' with 'e')
+enention -> exention (replace 'n' with 'x')
+exention -> exection (replace 'n' with 'c')
+exection -> execution (insert 'u')
  */
 public class EditDistance {
-	public int minDistance(String word1, String word2) {
+    public int minDistance(String word1, String word2) {
         int m = word1.length();
         int n = word2.length();
-        //dp[i][j] is edit distance of word1[0:i) and word2[0,j)
         int[][] dp = new int[m+1][n+1];
-        //first row
         for (int i=1;i<=n;i++) {
-            dp[0][i] = dp[0][i-1]+1;
+            dp[0][i] = i;
         }
-        //first col
         for (int i=1;i<=m;i++) {
-            dp[i][0] = dp[i-1][0]+1;
+            dp[i][0] = i;
         }
         for (int i=1;i<=m;i++) {
             for (int j=1;j<=n;j++) {
@@ -59,6 +49,9 @@ public class EditDistance {
                 }
             }
         }
+        // for (int[] arr:dp) {
+        //     System.out.println(Arrays.toString(arr));
+        // }
         return dp[m][n];
     }
 }
