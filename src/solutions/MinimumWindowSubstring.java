6f0546058d60e999846e715784b2ff88b4a2570b
ellinx <ellin.xll@gmail.com>
2019-03-15 20:40:35 -0700

    Update Minimum Window Substring.

--- src/solutions/MinimumWindowSubstring.java
+++ src/solutions/MinimumWindowSubstring.java
@@ -4,54 +4,55 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 
- * Given a string S and a string T, find the minimum window in S which will
- * contain all the characters in T in complexity O(n).
- * 
- * Example:
- * 
- * Input: S = "ADOBECODEBANC", T = "ABC" 
- * Output: "BANC" 
- * 
- * Note:
- * 
- * If there is no such window in S that covers all characters in T, return the empty string "". 
- * If there is such window, you are guaranteed that there will always be only one unique minimum window in S.
- * 
- * @author Ellinx
- *
+
+Given a string S and a string T, find the minimum window in S 
+which will contain all the characters in T in complexity O(n).
+
+Example:
+Input: S = "ADOBECODEBANC", T = "ABC"
+Output: "BANC"
+
+Note:
+1. If there is no such window in S that covers all characters in T, 
+	return the empty string "".
+2. If there is such window, you are guaranteed 
+	that there will always be only one unique minimum window in S.
  */
 public class MinimumWindowSubstring {
     public String minWindow(String s, String t) {
         Map<Character, Integer> counter = new HashMap<>();
         for (char c:t.toCharArray()) {
-            counter.put(c, counter.getOrDefault(c, 0)+1);
+            counter.put(c, counter.getOrDefault(c,0)+1);
         }
         int total = t.length();
         String ret = "";
-        int start = 0, end = 0;
-        while (end<s.length()) {
-            if (counter.containsKey(s.charAt(end))) {
-                int tmp = counter.get(s.charAt(end));
-                if (tmp>0) {
-                    total--;
-                }
-                counter.put(s.charAt(end), tmp-1);
-                while (total==0) {
-                    if (ret.length()==0 || ret.length()>end-start+1) {
-                        ret = s.substring(start,end+1);
-                    }
-                    if (counter.containsKey(s.charAt(start))) {
-                        tmp = counter.get(s.charAt(start));
-                        if (tmp>=0) {
-                            total++;
-                        }
-                        counter.put(s.charAt(start), tmp+1);
-                    }
-                    start++;
-                }
+        int l=0, r=0;
+        while (r<s.length()) {
+            char c = s.charAt(r);
+            if (!counter.containsKey(c)) {
+                r++;
+                continue;
             }
-            end++;
+            if (counter.get(c)>0) {
+                total--;
+            }
+            counter.put(c, counter.get(c)-1);
+            r++;
+            while (total==0) {
+                if (ret.length()==0 || ret.length()>r-l) {
+                    ret = s.substring(l,r);
+                }
+                c = s.charAt(l);
+                if (!counter.containsKey(c)) {
+                    l++;
+                    continue;
+                }
+                if (counter.get(c)>=0) {
+                    total++;
+                }
+                counter.put(c, counter.get(c)+1);
+                l++;
+            }
         }
         return ret;
     }
