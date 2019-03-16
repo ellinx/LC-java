373381b31c9ae2b71d5729bb3b9f8dfbb08d1acc
ellinx <ellin.xll@gmail.com>
2019-03-15 20:30:15 -0700

    Update Trapping Rain Water.

--- src/solutions/TrappingRainWater.java
+++ src/solutions/TrappingRainWater.java
@@ -1,37 +1,32 @@
 package solutions;
 
 /**
- * 
- * Given n non-negative integers representing an elevation map where the width
- * of each bar is 1, compute how much water it is able to trap after raining.
- * 
- * 
- * The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In
- * this case, 6 units of rain water (blue section) are being trapped. Thanks
- * Marcos for contributing this image!
- * 
- * Example:
- * 
- * Input: [0,1,0,2,1,0,1,3,2,1,2,1] Output: 6
- * 
- * @author Ellinx
- *
+Given n non-negative integers representing an elevation map where the width of each bar is 1, 
+compute how much water it is able to trap after raining.
+
+The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. 
+In this case, 6 units of rain water (blue section) are being trapped.
+
+Example:
+Input: [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
  */
 public class TrappingRainWater {
-	public int trap(int[] height) {
-		int[] left = new int[height.length];
-		int[] right = new int[height.length];
-		for (int i = 1; i < height.length; i++) {
-			left[i] = Math.max(left[i - 1], height[i - 1]);
-			right[height.length - 1 - i] = Math.max(right[height.length - i], height[height.length - i]);
-		}
-		int ret = 0;
-		for (int i = 0; i < height.length; i++) {
-			int temp = Math.min(left[i], right[i]);
-			if (temp > height[i]) {
-				ret += temp - height[i];
-			}
-		}
-		return ret;
-	}
+    public int trap(int[] height) {
+        int n = height.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        for (int i=1;i<n;i++) {
+            left[i] = Math.max(left[i-1],height[i-1]);
+            right[n-1-i] = Math.max(right[n-i],height[n-i]);
+        }
+        int ret = 0;
+        for (int i=1;i<n-1;i++) {
+            int water = Math.min(left[i],right[i])-height[i];
+            if (water>0) {
+                ret += water;
+            }
+        }
+        return ret;
+    }
 }
