b60308220e2591425a9b97ad1628c209c76e8b5b
ellinx <ellin.xll@gmail.com>
2019-03-16 22:14:09 -0700

    Update Jump Game II.

--- src/solutions/JumpGameII.java
+++ src/solutions/JumpGameII.java
@@ -1,50 +1,42 @@
 package solutions;
 
 /**
- * 
- * Given an array of non-negative integers, you are initially positioned at the
- * first index of the array.
- * 
- * Each element in the array represents your maximum jump length at that
- * position.
- * 
- * Your goal is to reach the last index in the minimum number of jumps.
- * 
- * Example:
- * 
- * Input: [2,3,1,1,4] 
- * Output: 2 
- * Explanation: The minimum number of jumps to reach the last index is 2. 
- * Jump 1 step from index 0 to 1, then 3 steps to the last index.
- * 
- * Note:
- * 
- * You can assume that you can always reach the last index.
- * 
- * @author Ellinx
- *
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Your goal is to reach the last index in the minimum number of jumps.
+
+Example:
+
+Input: [2,3,1,1,4]
+Output: 2
+Explanation: The minimum number of jumps to reach the last index is 2.
+    			Jump 1 step from index 0 to 1, then 3 steps to the last index.
+    
+Note:
+You can assume that you can always reach the last index.
  */
 public class JumpGameII {
     public int jump(int[] nums) {
-        int index = 0;
-        int curMax = 0;
-        int step = 0;
-        if (curMax>=nums.length-1) {
-            return step;
+        if (nums.length==1) {
+            return 0;
         }
-        while (index<=curMax) {
-            int nextMax = curMax;
-            while (index<=curMax) {
-                nextMax = Math.max(nextMax, index+nums[index]);
-                index++;
+        int cur = 1;
+        int reach = nums[0];
+        int step = 1;
+        while (cur<=reach) {
+            if (reach>=nums.length-1) {
+                return step;
             }
+            int nxtReach = reach;
+            while (cur<=reach) {
+                nxtReach = Math.max(nxtReach, cur+nums[cur]);
+                cur++;
+            }
+            reach = nxtReach;
             step++;
-            curMax = nextMax;
-            //System.out.println(index+","+curMax+","+step);
-            if (curMax>=nums.length-1) {
-                break;
-            }
         }
-        return step;
+        return -1;
     }
 }
