a4da63084ef662716b9048f027f803b2a7ee4abf
ellinx <ellin.xll@gmail.com>
2019-04-02 20:59:17 -0700

    Update Binary Tree Maximum Path Sum.

--- src/solutions/BinaryTreeMaximumPathSum.java
+++ src/solutions/BinaryTreeMaximumPathSum.java
@@ -1,21 +1,31 @@
 package solutions;
 
 /**
- * Given a binary tree, find the maximum path sum.
- * 
- * For this problem, a path is defined as any sequence of nodes from some
- * starting node to any node in the tree along the parent-child connections. The
- * path must contain at least one node and does not need to go through the root.
- * 
- * For example: Given the below binary tree,
- * 
- *    1 
- *   / \
- *  2   3 
- *  
- * Return 6.
- * 
- *
+Given a non-empty binary tree, find the maximum path sum.
+
+For this problem, a path is defined as any sequence of nodes from some starting node 
+to any node in the tree along the parent-child connections. 
+The path must contain at least one node and does not need to go through the root.
+
+Example 1:
+Input: [1,2,3]
+
+       1
+      / \
+     2   3
+
+Output: 6
+
+Example 2:
+Input: [-10,9,20,null,null,15,7]
+
+   -10
+   / \
+  9  20
+    /  \
+   15   7
+
+Output: 42
  */
 public class BinaryTreeMaximumPathSum {
 	int maxSum;
