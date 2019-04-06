392c9c9d4765604ba6d1a6193725967dea5692c7
ellinx <ellin.xll@gmail.com>
2019-04-05 20:48:49 -0700

    Update Sqrt(x).

--- src/solutions/Sqrt.java
+++ src/solutions/Sqrt.java
@@ -22,20 +22,16 @@ Explanation: The square root of 8 is 2.82842..., and since
 
 public class Sqrt {
     public int mySqrt(int x) {
-        long l=0, r=x;
+        long l=0,r=x;
         while (l<r) {
-            long m = l+(r-l)/2;
-            if (m*m==x) {
-                return (int)m;
+            long mid = l+(r-l)/2;
+            if (mid*mid<=x && (mid+1)*(mid+1)>x) {
+                return (int)mid;
             }
-            if (m*m<x) {
-                if (m+1<=r && (m+1)*(m+1)<=x) {
-                    l = m+1;
-                    continue;
-                }
-                return (int)m;
+            if (mid*mid<x) {
+                l = mid+1;
             } else {
-                r = m-1;
+                r = mid-1;
             }
         }
         return (int)l;
