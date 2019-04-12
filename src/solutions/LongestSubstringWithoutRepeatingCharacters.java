7ff2fc408cc6073d16f20a0cbe664a4fca6ceb05
ellinx <ellin.xll@gmail.com>
2019-04-11 21:28:50 -0700

    Update Longest Substring Without Repeating Characters.

--- src/solutions/LongestSubstringWithoutRepeatingCharacters.java
+++ src/solutions/LongestSubstringWithoutRepeatingCharacters.java
@@ -4,49 +4,39 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * Given a string, find the length of the longest substring without repeating
- * characters.
- * 
- * Examples:
- * 
- * Given "abcabcbb", the answer is "abc", which the length is 3.
- * 
- * Given "bbbbb", the answer is "b", with the length of 1.
- * 
- * Given "pwwkew", the answer is "wke", with the length of 3. Note that the
- * answer must be a substring, "pwke" is a subsequence and not a substring.
- * 
- * @author Ellinx
- *
+Given a string, find the length of the longest substring without repeating characters.
+
+Example 1:
+Input: "abcabcbb"
+Output: 3 
+Explanation: The answer is "abc", with the length of 3. 
+
+Example 2:
+Input: "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+
+Example 3:
+Input: "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3. 
+             Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
  */
 public class LongestSubstringWithoutRepeatingCharacters {
-	public int lengthOfLongestSubstring(String s) {
-        if (s.length()==0)
-            return 0;
-        
-        int res = 1;
-        int start = 0;
-        int end = 0;
+    public int lengthOfLongestSubstring(String s) {
         Set<Character> set = new HashSet<>();
-        
-        while (end < s.length()) {
-            while (end < s.length() && !set.contains(s.charAt(end))) {
-                set.add(s.charAt(end));
-                end++;
+        int l=0, r=0;
+        int ret = 0;
+        while (r<s.length()) {
+            while (set.contains(s.charAt(r))) {
+                set.remove(s.charAt(l));
+                l++;
             }
-            res = Math.max(res, end-start);
-            //System.out.println("start="+start+",end="+end);
-            if (end==s.length())
-                return res;
-            
-            while (s.charAt(start)!=s.charAt(end)) {
-                set.remove(s.charAt(start));
-                start++;
-            }
-            start++;
-            end++;
+            set.add(s.charAt(r));
+            r++;
+            ret = Math.max(ret, r-l);
         }
-        return res;
+        return ret;
     }
 	
 	//test
