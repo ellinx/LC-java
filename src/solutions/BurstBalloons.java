79387172866e1eec3e217e08fdc2bfd34530da6a
ellinx <ellin.xll@gmail.com>
2019-04-15 21:03:45 -0700

    Update Burst Balloons.

--- src/solutions/BurstBalloons.java
+++ src/solutions/BurstBalloons.java
@@ -1,24 +1,24 @@
 package solutions;
 
 /**
- * 
- * Given n balloons, indexed from 0 to n-1. Each balloon is painted with a number on it represented by array nums. You are asked to burst all the balloons. If the you burst balloon i you will get nums[left] * nums[i] * nums[right] coins. Here left and right are adjacent indices of i. After the burst, the left and right then becomes adjacent.
- * 
- * Find the maximum coins you can collect by bursting the balloons wisely.
- * 
- * Note:
- * 
- *     You may imagine nums[-1] = nums[n] = 1. They are not real therefore you can not burst them.
- *     0 ≤ n ≤ 500, 0 ≤ nums[i] ≤ 100
- * 
- * Example:
- * 
- * Input: [3,1,5,8]
- * Output: 167 
- * Explanation: nums = [3,1,5,8] --> [3,5,8] -->   [3,8]   -->  [8]  --> []
- *              coins =  3*1*5      +  3*5*8    +  1*3*8      + 1*8*1   = 167
- * 
- * 
+Given n balloons, indexed from 0 to n-1. 
+Each balloon is painted with a number on it represented by array nums. 
+You are asked to burst all the balloons. 
+If the you burst balloon i you will get nums[left] * nums[i] * nums[right] coins. 
+Here left and right are adjacent indices of i. 
+After the burst, the left and right then becomes adjacent.
+
+Find the maximum coins you can collect by bursting the balloons wisely.
+
+Note:
+1. You may imagine nums[-1] = nums[n] = 1. They are not real therefore you can not burst them.
+2. 0 ≤ n ≤ 500, 0 ≤ nums[i] ≤ 100
+
+Example:
+Input: [3,1,5,8]
+Output: 167 
+Explanation: nums = [3,1,5,8] --> [3,5,8] -->   [3,8]   -->  [8]  --> []
+             coins =  3*1*5      +  3*5*8    +  1*3*8      + 1*8*1   = 167
  */
 public class BurstBalloons {
 	/**
@@ -30,27 +30,27 @@ public class BurstBalloons {
 	 * Space: O(n^2)
 	 */
     public int maxCoins(int[] nums) {
-        int[] numsWithBoundary = new int[nums.length+2];
-        numsWithBoundary[0] = 1;
-        for (int i=1;i<numsWithBoundary.length-1;i++) {
-            numsWithBoundary[i] = nums[i-1];
+        int n = nums.length;
+        int[] newNums = new int[n+2];
+        newNums[0] = 1;
+        newNums[n+1] = 1;
+        for (int i=0;i<n;i++) {
+            newNums[i+1] = nums[i];
         }
-        numsWithBoundary[numsWithBoundary.length-1] = 1;
-        int[][] mm = new int[numsWithBoundary.length][numsWithBoundary.length];
-        return dfs(numsWithBoundary,0,numsWithBoundary.length-1,mm);
+        int[][] mm = new int[n+2][n+2];
+        return dfs(newNums, 0, n+1, mm);
     }
-    private int dfs(int[] nums, int start, int end, int[][] mm) {
-        if (end-start==2) {
-            return nums[start]*nums[start+1]*nums[end];
-        }
-        if (mm[start][end]>0) {
-            return mm[start][end];
+    private int dfs(int[] nums, int left, int right, int[][] mm) {
+        if (mm[left][right]>0) {
+            return mm[left][right];
         }
         int ret = 0;
-        for (int i=start+1;i<end;i++) {
-            ret = Math.max(ret, nums[start]*nums[i]*nums[end] + dfs(nums,start,i,mm) + dfs(nums,i,end,mm));
+        for (int i=left+1;i<right;i++) {
+            int temp = nums[left]*nums[i]*nums[right];
+            temp += dfs(nums,left,i,mm)+dfs(nums,i,right,mm);
+            ret = Math.max(ret, temp);
         }
-        mm[start][end] = ret;
+        mm[left][right] = ret;
         return ret;
     }
 }
