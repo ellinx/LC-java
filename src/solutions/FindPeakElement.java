8374988e256704cbfe81ce071cff9603956f55bd
boboran <liuboran0413@gmail.com>
2019-05-13 23:05:45 -0700

    Update Find Peak Element.

--- src/solutions/FindPeakElement.java
+++ src/solutions/FindPeakElement.java
@@ -1,48 +1,42 @@
 package solutions;
 
 /**
- * A peak element is an element that is greater than its neighbors.
- * 
- * Given an input array nums, where nums[i] ≠ nums[i+1], find a peak element and
- * return its index.
- * 
- * The array may contain multiple peaks, in that case return the index to any
- * one of the peaks is fine.
- * 
- * You may imagine that nums[-1] = nums[n] = -∞.
- * 
- * Example 1:
- * 
- * Input: nums = [1,2,3,1] 
- * Output: 2 
- * Explanation: 3 is a peak element and your function should return the index number 2.
- * 
- * Example 2:
- * 
- * Input: nums = [1,2,1,3,5,6,4] 
- * Output: 1 or 5 
- * Explanation: Your function can return either index number 1 where the peak element is 2, 
- * or index number 5 where the peak element is 6.
- * 
- * Note:
- * 
- * Your solution should be in logarithmic complexity.
- * 
- * @author Ellinx
- *
+ A peak element is an element that is greater than its neighbors.
+
+ Given an input array nums, where nums[i] ≠ nums[i+1], find a peak element and return its index.
+
+ The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+
+ You may imagine that nums[-1] = nums[n] = -∞.
+
+ Example 1:
+ Input: nums = [1,2,3,1]
+ Output: 2
+ Explanation: 3 is a peak element and your function should return the index number 2.
+
+ Example 2:
+ Input: nums = [1,2,1,3,5,6,4]
+ Output: 1 or 5
+ Explanation: Your function can return either index number 1 where the peak element is 2,
+ or index number 5 where the peak element is 6.
+
+ Note:
+ Your solution should be in logarithmic complexity.
  */
 public class FindPeakElement {
     public int findPeakElement(int[] nums) {
-        int start = 0;
-        int end = nums.length-1;
-        while (start<end) {
-            int mid = start+(end-start)/2;
-            if (nums[mid]>nums[mid+1]) {
-                end = mid;
+        int l=0, r=nums.length-1;
+        while (l<=r) {
+            int m = l+(r-l)/2;
+            if ((m==l || nums[m-1]<nums[m]) && (m==r || nums[m]>nums[m+1])) {
+                return m;
+            }
+            if (m==l || nums[m-1]<nums[m]) {
+                l = m+1;
             } else {
-                start = mid+1;
+                r = m-1;
             }
         }
-        return start;
+        return l;
     }
 }
